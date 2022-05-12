b663a094984872f8b360a63de272b81f5d9552e4
daichunlin <307177635@qq.com>
2022-05-12 20:40:37 +0800

    feat: 添加校验

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author       : daichunlin
+ * @Date         : 2022-05-12 20:33:44
+ * @LastEditors  : daichunlin
+ * @LastEditTime : 2022-05-12 20:34:26
+ * @Description  : file content
+ * @FilePath     : /vue3-project/commitlint.config.js
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] }
